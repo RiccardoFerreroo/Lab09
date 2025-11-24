bbd2cd7832f8b74d87dcb86402ef7df24d89e052
RiccardoFerreroo <s336535@studenti.polito.it>
2025-11-24 12:51:27 -1000

    my_lab_09

--- database/attrazione_DAO.py
+++ database/attrazione_DAO.py
@@ -17,7 +17,7 @@ class AttrazioneDAO:
             return None
 
         cursor = cnx.cursor(dictionary=True)
-        query = """ ADD YOUR QUERY """ # TODO
+        query = """ select * from attrazione """
         try:
             cursor.execute(query)
             for row in cursor:
